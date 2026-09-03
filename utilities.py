75a37598fca48bf53c68922078b3b717513791b7
ventus34 <ventus34@users.noreply.github.com>
2026-09-03 19:27:58 +0000

    Add missing imports os, sys, copy, and logging in utilities.py and exporter.py

--- utilities.py
+++ utilities.py
@@ -14,7 +14,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+import os
+import sys
+import copy
+import logging
+from logging import warning, info, error
 import torch
 from torch.cuda import nvtx
 from collections import OrderedDict
